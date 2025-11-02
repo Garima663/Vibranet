506c08c23c93fc575ffa94d980334b92d8ccd5a1
Garima663 <srivastavagarima226@gmail.com>
2025-11-02 13:32:04 +0530

    Update navbar logo text from Streamify to Vibranet and modify User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -28,6 +28,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "",
    },
+   learningLanguage: {
+      type: String,
+      default: "",
+    },
    isOnboarded: {
     type: Boolean,
     default: false,
